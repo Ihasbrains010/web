cef7769ada94b577f36c44fb6d287077e1187cea
manvikkhatri111 <manvikkhatri4@gmail.com>
2025-05-12 21:16:17 -1200

    Switch to HashRouter for GitHub Pages compatibility

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { CartProvider } from './context/CartContext';
@@ -8,11 +8,11 @@ import { CartProvider } from './context/CartContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
